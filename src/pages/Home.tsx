64a229092b6780f253b4c444147ed900bf35a207
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-01-29 03:17:11 -0300

    chore: added styles to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import '../styles/auth.scss'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -11,25 +13,22 @@ export function Home() {
           alt="illustration symbolizing questions and answers"
         />
         <strong>Create live Q&amp;A rooms</strong>
-        <p>take questions from your audience in real time</p>
-        <main>
-          <div>
-            <img src={logoImg} alt="Letmeask Google Sign In" />
-            <button>
-              <img src={googleIconImg} alt="Google brand" />
-              Create your room with Google
-            </button>
-            <div>or enter a room</div>
-            <form>
-              <input
-                type="text"
-                placeholder="Enter room code"
-              />
-              <button type="submit">Enter in the room</button>
-            </form>
-          </div>
-        </main>
+        <p>Take questions from your audience in real time</p>
       </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask Google Sign In" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Google brand" />
+            Create your room with Google
+          </button>
+          <div className="separator">or enter a room</div>
+          <form>
+            <input type="text" placeholder="Enter room code" />
+            <button type="submit">Enter in the room</button>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }

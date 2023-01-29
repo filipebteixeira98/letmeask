60db4b19e2ea60f751a9ada65d1664ef333f1755
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-01-29 03:42:10 -0300

    chore: created new room page layout

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,34 @@
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="illustration symbolizing questions and answers"
+        />
+        <strong>Create live Q&amp;A rooms</strong>
+        <p>Take questions from your audience in real time</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask Google Sign In" />
+          <h2>Create a new room</h2>
+          <form>
+            <input type="text" placeholder="Room name" />
+            <Button type="submit">Create room</Button>
+          </form>
+          <p>
+            Do you want to enter a existing room? <a href="#">Click here</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
